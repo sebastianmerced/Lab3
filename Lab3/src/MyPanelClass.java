c5a7b87fb13e9dea29c8d0ac05f4b6a935dbf23a
4015a9 <4015a9@icaco.ece.uprm.edu>
2017-11-15 13:00:39 -0400

    centered oval shape

---
+++ Lab3/src/MyPanelClass.java
@@ -0,0 +1,46 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Insets;
+import javax.swing.JPanel;
+ 
+public class MyPanelClass extends JPanel {
+            /**
+	 * 
+	 */
+	private static final long serialVersionUID = 7057541440811488699L;
+
+			public void paintComponent(Graphics g) {
+                        super.paintComponent(g);
+ 
+                        //Compute interior coordinates
+                        Insets myInsets = this.getInsets();
+                        int x1 = myInsets.left;
+                        int y1 = myInsets.top;
+                        int x2 = getWidth() - myInsets.right - 1;
+                        int y2 = getHeight() - myInsets.bottom - 1;
+                        int width = x2 - x1;
+                        int height = y2 - y1;
+ 
+                        //Paint the background
+                        g.setColor(Color.MAGENTA);
+                        g.fillRect(x1, y1, width + 1, height + 1);
+//                      //Draw a border
+//                        g.setColor(Color.YELLOW);
+//                        g.drawRect(x1, y1, width, height);
+//                        
+//                        // draws an inside border
+//                        g.setColor(Color.BLACK);
+//                        g.drawRect(x1 + 1, y1+1, width-2, height-2);
+//                        
+//                        // draws line
+//                        g.setColor(Color.BLACK);
+//                        g.drawLine(x1, y1, x2, y2);
+//                        
+//                        g.setColor(Color.CYAN);
+//                        g.drawLine(x2, y1, x1, y2);
+                        
+                        // draws an oval
+                        g.setColor(Color.LIGHT_GRAY);
+                        g.fillOval((int)(width -55)/2, (int)(height-55)/2, 55, 55);
+            }
+}
